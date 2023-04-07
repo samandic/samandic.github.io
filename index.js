d46dd7bb3e654da0f97adedc72efd406bf3e0927
samandic <60607234+samandic@users.noreply.github.com>
2023-04-07 09:28:42 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -13,6 +13,24 @@ app.post("/insertEvent", (req, res) => {
 
 function insertEvent(event) {
     console.log(event.data);
+    logJSONData();
+}
+async function logJSONData() {
+  fetch('https://api.telegram.org/bot5898404192:AAEoxoZ-X4hF3eHfu1g1Sg0NdNhc3OPaP0s/sendMessage', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    body: '{"chat_id":-1001911823358,"text":"Hello from Github"}'//JSON.stringify({a: 7, str: 'Some string: &=&'})
+  }).then(res => res.json())
+    .then(res => console.log(res));
 }
 
 app.listen(process.env.PORT || 3000)
+
+/*l_result := APEX_WEB_SERVICE.MAKE_REST_REQUEST(
+    p_url => 'https://api.telegram.org/bot5898404192:AAEoxoZ-X4hF3eHfu1g1Sg0NdNhc3OPaP0s/sendMessage'
+   ,p_http_method => 'POST'
+   ,p_body => '{"chat_id":-1001911823358,"text":"Welcome to https://apex18.blogspot.com. Introducing https://apexpert.in."}'
+  );*/
