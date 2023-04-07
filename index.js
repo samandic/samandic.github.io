e37f2645972c9f1f264eb8bf9357cb357a988d14
samandic <60607234+samandic@users.noreply.github.com>
2023-04-07 09:08:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
+app.use(express.json());
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.post("/insertEvent", (req, res) => {
+  console.log(req.body); // your JSON
+  var result = insertEvent(req.body);
+  res.send(req.body);
+});
+
+function insertEvent(event) {
+    console.log(event.data);
+}
+
 app.listen(process.env.PORT || 3000)
